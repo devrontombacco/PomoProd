6aaf72fcf588821ab148738e9fbe44cd1352e590
Devron Tombacco <tombacco.devron@gmail.com>
2021-01-11 15:35:43 +0000

    Add Constants file

---
+++ PomoProd/PomoProd/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  PomoProd
+//
+//  Created by Devron Tombacco on 11/01/2021.
+//
+
+import UIKit
+
+struct K {
+    struct color {
+        static let highlighted = UIColor(red: 13/255, green: 255/255, blue: 239/255, alpha: 100).cgColor
+    }
+}
