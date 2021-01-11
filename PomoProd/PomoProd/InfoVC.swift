979e9fb5e59183bb87a7a8a9130a3307c463db70
Devron Tombacco <tombacco.devron@gmail.com>
2021-01-11 15:36:07 +0000

    Finish word highlights in InfoVC

--- PomoProd/PomoProd/InfoVC.swift
+++ PomoProd/PomoProd/InfoVC.swift
@@ -9,12 +9,24 @@ import UIKit
 
 class InfoVC: UIViewController {
 
+    // Container
     @IBOutlet weak var instructionsContainerView: UIView!
+    
+    // Title
     @IBOutlet weak var instructionsTitleLabel: UILabel!
+    
+    // Panels
     @IBOutlet weak var pomodoroPanel: UIView!
     @IBOutlet weak var shortBreakPanel: UIView!
     @IBOutlet weak var longBreakPanel: UIView!
     @IBOutlet weak var productivityPanel: UIView!
+    
+    // Labels
+    @IBOutlet weak var pomodoPanelLabel: UILabel!
+    @IBOutlet weak var shortBreakPanelLabel: UILabel!
+    @IBOutlet weak var longBreakPanelLabel: UILabel!
+    @IBOutlet weak var productivityPanelLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +37,8 @@ class InfoVC: UIViewController {
         longBreakPanel.layer.cornerRadius = 25
         productivityPanel.layer.cornerRadius = 25
         configureShadows()
+        
+        configurePanelTextColors()
     }
     
     func configureShadows(){
@@ -51,4 +65,29 @@ class InfoVC: UIViewController {
         
     }
 
+    func configurePanelTextColors(){
+
+        let pomodoroHighlight = "A pomodoro is a unit of time, typically 25 minutes, that you work for without any breaks"
+        let pomodoroHighlightAttributedString = NSMutableAttributedString(string: pomodoroHighlight)
+        pomodoroHighlightAttributedString.addAttribute(.foregroundColor, value: K.color.highlighted, range: NSRange(location: 2, length: 8))
+        pomodoPanelLabel.attributedText = pomodoroHighlightAttributedString
+        
+        let shortBreakHighlight = "Between each pomodoro you may take a short break (it is recommended not to do longer than 5 minutes)"
+        let shortBreakHighlightAttributedString = NSMutableAttributedString(string: shortBreakHighlight)
+        shortBreakHighlightAttributedString.addAttribute(.foregroundColor, value: K.color.highlighted, range: NSRange(location: 36, length: 12))
+        shortBreakPanelLabel.attributedText = shortBreakHighlightAttributedString
+        
+            
+        let longBreakHighlight = "After a series of pomodoros (usually 3 or 4), you can then take a long break for about 25 minutes"
+        let longBreakHighlightAttributedString = NSMutableAttributedString(string: longBreakHighlight)
+        longBreakHighlightAttributedString.addAttribute(.foregroundColor, value: K.color.highlighted, range: NSRange(location: 66, length: 10))
+        longBreakPanelLabel.attributedText = longBreakHighlightAttributedString
+        
+        let productiveHighlight = "These short bursts of focus prove to be much more productive in the long run!"
+        let productiveHighlightAttributedString = NSMutableAttributedString(string: productiveHighlight)
+        productiveHighlightAttributedString.addAttribute(.foregroundColor, value: K.color.highlighted, range: NSRange(location: 50, length: 10))
+        productivityPanelLabel.attributedText = productiveHighlightAttributedString
+    
+    }
+    
 }
